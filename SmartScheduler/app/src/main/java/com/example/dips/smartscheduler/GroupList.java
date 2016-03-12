3e0de1b6c5d88c0d95d01bee8a592b7033fbfeba
Jorge Martinez <jorgem1398@gmail.com>
2016-03-11 18:39:59 -0600

    Added stubs for all the activities

---
+++ SmartScheduler/app/src/main/java/com/example/dips/smartscheduler/GroupList.java
@@ -0,0 +1,13 @@
+package com.example.dips.smartscheduler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GroupList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_list);
+    }
+}
