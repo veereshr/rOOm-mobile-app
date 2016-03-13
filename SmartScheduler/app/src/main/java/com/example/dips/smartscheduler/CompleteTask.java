bafd034b66fa7eb9dab310bed6977b4ec7eb27d2
vrangasa <rveereshwaran@gmail.com>
2016-03-12 22:02:42 -0600

    Java - ViewSingleTask, CompleteTask

--- SmartScheduler/app/src/main/java/com/example/dips/smartscheduler/CompleteTask.java
+++ SmartScheduler/app/src/main/java/com/example/dips/smartscheduler/CompleteTask.java
@@ -1,7 +1,9 @@
 package com.example.dips.smartscheduler;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CompleteTask extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class CompleteTask extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complete_task);
+    }
+
+    public void backToViewTasks (View view){
+        Intent intent = new Intent(getApplicationContext(), ViewTask.class);
+        startActivity(intent);
     }
 }
--- SmartScheduler/app/src/main/java/com/example/dips/smartscheduler/ViewSingleTask.java
+++ SmartScheduler/app/src/main/java/com/example/dips/smartscheduler/ViewSingleTask.java
@@ -1,13 +1,32 @@
 package com.example.dips.smartscheduler;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class ViewSingleTask extends AppCompatActivity {
 
+
+    Button btCompleteTask;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_single_task);
+        findViewsById();
+
+
+
+    }
+
+    private void findViewsById() {
+        btCompleteTask = (Button) findViewById(R.id.btCompleteTask);
+
+    }
+
+    public void completeTaskBtn(View view){
+        Intent intent = new Intent(getApplicationContext(), CompleteTask.class);
+        startActivity(intent);
     }
 }
