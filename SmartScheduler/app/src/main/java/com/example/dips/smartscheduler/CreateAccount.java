eb12032fc83f7fd4e33db37615e096113522b3ab
Jorge Martinez <jorgem1398@gmail.com>
2016-03-31 18:03:28 -0500

    Updated createUser UI

--- SmartScheduler/app/src/main/java/com/example/dips/smartscheduler/CreateAccount.java
+++ SmartScheduler/app/src/main/java/com/example/dips/smartscheduler/CreateAccount.java
@@ -1,10 +1,13 @@
 package com.example.dips.smartscheduler;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CreateAccount extends AppCompatActivity {
 
@@ -12,33 +15,47 @@ public class CreateAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        //TODO get users phonenumber
     }
 
     public void CreateAccount(View v){
         //getdata
-        String pNum = ((TextView)findViewById(R.id.phoneText)).getText().toString();
-        String pass = ((TextView)findViewById(R.id.passText)).getText().toString();
-        String passCon = ((TextView)findViewById(R.id.passConText)).getText().toString();
-        String fName = ((TextView)findViewById(R.id.fNameText)).getText().toString();
-        String lName = ((TextView)findViewById(R.id.lNameText)).getText().toString();
-        String email = ((TextView)findViewById(R.id.emailText)).getText().toString();
+        String pNum = "";
+        String pass = "";
+        String passCon = "";
+        String fName = "";
+        String lName = "";
+        String email = "";
+        pNum = ((TextView)findViewById(R.id.phoneText)).getText().toString();
+        pass = ((TextView)findViewById(R.id.passText)).getText().toString();
+        passCon = ((TextView)findViewById(R.id.passConText)).getText().toString();
+        fName = ((TextView)findViewById(R.id.fNameText)).getText().toString();
+        lName = ((TextView)findViewById(R.id.lNameText)).getText().toString();
+        email = ((TextView)findViewById(R.id.emailText)).getText().toString();
 
         //validate Data
-        //TODO check if phone number
-        //TODO confirm that password and passwordConfirm all both the same
+        //Removes all non numbers from phonenumber
+        pNum = pNum.replaceAll("[^\\d.]", "");
+        ((TextView)findViewById(R.id.phoneText)).setText(pNum);
+        //check phonenumber length
+        if(pNum.length() < 1){
+            Toast.makeText(this,"Phone number can not be empty",Toast.LENGTH_SHORT).show();
+        }else{
+            //check if password match
+            if(!pass.equals(passCon))
+            {
+                Toast.makeText(this,"Passwords do not match",Toast.LENGTH_SHORT).show();
+            }else{
+                //TODO CHECK IF USER ALREADY EXIST
 
-        pNum = "773-397-8220";
-        pass = "PassWord";
-        fName = "Jorge";
-        lName = "Martinez";
-        email = "ME@ME.com";
+                //save to db
+                DatabaseHelper dbhelper = new DatabaseHelper(this);
+                dbhelper.InsertNewUser(pNum,pass,fName,lName,email);
 
-        //save to db
-        DatabaseHelper dbhelper = new DatabaseHelper(this);
-        dbhelper.InsertNewUser(pNum,pass,fName,lName,email);
-
-        //passing intent
-        Intent intent = new Intent(v.getContext(),GroupList.class);
-        startActivity(intent);
+                //passing intent
+                Intent intent = new Intent(v.getContext(),GroupList.class);
+                startActivity(intent);
+            }
+        }
     }
 }
