b5494a0f3bce76f39d38769254da1a3b6d9b0cf3
Jorge Martinez <jorgem1398@gmail.com>
2016-04-06 13:10:59 -0500

    Added UI to create Task

--- SmartScheduler/app/src/main/java/com/example/dips/smartscheduler/CompleteTask.java
+++ SmartScheduler/app/src/main/java/com/example/dips/smartscheduler/CompleteTask.java
@@ -1,19 +1,144 @@
 package com.example.dips.smartscheduler;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import android.database.Cursor;
+import android.util.Log;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 
 public class CompleteTask extends AppCompatActivity {
+
+    private Bitmap curimage;
+    private ArrayList imageList = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complete_task);
+        //TODO GET CURRENT TASK
+        ((TextView) findViewById(R.id.completeTaskTitle)).setText("Title");
+        ((TextView) findViewById(R.id.completeTaskDesc)).setText("Description");
+        ((TextView) findViewById(R.id.completeTaskDue)).setText("03/04/2016");
+        ((TextView) findViewById(R.id.completeTaskFinish)).setText("03/06/2016");
     }
 
-    public void backToViewTasks (View view){
+    public void UploadImage(View view){
+
+        final CharSequence[] options = { "Take Photo", "Choose from Gallery","Cancel" };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add Photo!");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (options[item].equals("Take Photo")) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    File f = new File(android.os.Environment.getExternalStorageDirectory(), "temp.jpg");
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                    startActivityForResult(intent, 1);
+                } else if (options[item].equals("Choose from Gallery")) {
+                    Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent, 2);
+
+                } else if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                curimage = null;
+                File f = new File(Environment.getExternalStorageDirectory().toString());
+                for (File temp : f.listFiles()) {
+                    if (temp.getName().equals("temp.jpg")) {
+                        f = temp;
+                        break;
+                    }
+                }
+                try {
+                    BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+
+                    curimage = BitmapFactory.decodeFile(f.getAbsolutePath(),
+                            bitmapOptions);
+
+                    String path = android.os.Environment
+                            .getExternalStorageDirectory()
+                            + File.separator
+                            + "Phoenix" + File.separator + "default";
+                    f.delete();
+                    OutputStream outFile = null;
+                    File file = new File(path, String.valueOf(System.currentTimeMillis()) + ".jpg");
+                    try {
+                        outFile = new FileOutputStream(file);
+                        curimage.compress(Bitmap.CompressFormat.JPEG, 85, outFile);
+                        outFile.flush();
+                        outFile.close();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else if (requestCode == 2) {
+
+                Uri selectedImage = data.getData();
+                String[] filePath = { MediaStore.Images.Media.DATA };
+                Cursor c = getContentResolver().query(selectedImage,filePath, null, null, null);
+                c.moveToFirst();
+                int columnIndex = c.getColumnIndex(filePath[0]);
+                String picturePath = c.getString(columnIndex);
+                c.close();
+                curimage = (BitmapFactory.decodeFile(picturePath));
+                Log.w("CompleteTask", picturePath + "");
+            }
+            //if image is selected
+            if(curimage != null){
+                curimage = Bitmap.createScaledBitmap(curimage, 500, 500, true);
+                ImageView iv = new ImageView(this);
+                iv.setImageBitmap(curimage);
+                iv.setPadding(10,10, 10, 10);
+                imageList.add(curimage);
+                ((LinearLayout) findViewById(R.id.completeTaskImageLayout)).addView(iv);
+            }
+
+        }
+    }
+
+    public void CompleteTest(View view){
+        //TODO
+        backToViewTasks();
+    }
+
+    public void backToViewTasks (){
         Intent intent = new Intent(getApplicationContext(), ViewTask.class);
         startActivity(intent);
     }
--- SmartScheduler/app/src/main/java/com/example/dips/smartscheduler/CreateTask.java
+++ SmartScheduler/app/src/main/java/com/example/dips/smartscheduler/CreateTask.java
@@ -1,72 +1,200 @@
 package com.example.dips.smartscheduler;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class CreateTask extends AppCompatActivity {
+
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private int year, month, day;
+    private Bitmap curimage;
+    private ArrayList imageList = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_task);
 
+        //get todays date and put in select date text
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        showDate(year, month, day);
 
-      //  ExpandableListView myListView= (ExpandableListView) findViewById(R.id.expandableListView);
-
-       // String[] groups=new String[]{"Room:314,College,GymBuddies"};
-        List<String> listGroupItems=new ArrayList<String>();
-
-        listGroupItems.add("Room:314");
-        listGroupItems.add("Room:315");
-        listGroupItems.add("Room:316");
-        listGroupItems.add("Room:317");
-
-        Spinner dropdown = (Spinner)findViewById(R.id.dropDownGroupName);
-       // String[] items = new String[]{"1", "2", "three"};
-        ArrayAdapter<String> adapterGroupName = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, listGroupItems);
+        //TODO get current group team members and fill spinner
+        Spinner dropdown = (Spinner)findViewById(R.id.createTaskTeam);
+        List<String> listGroupItems=new ArrayList<>();
+        listGroupItems.add("");
+        ArrayAdapter<String> adapterGroupName = new ArrayAdapter<>(
+                this, android.R.layout.simple_spinner_dropdown_item, listGroupItems);
         dropdown.setAdapter(adapterGroupName);
+    }
 
-        ListView myFriendList=(ListView)findViewById(R.id.friendsListView);
+    //used for Select Date
+    @SuppressWarnings("deprecation")
+    public void GetDate(View view){
+        showDialog(999);
+    }
+    @SuppressWarnings("deprecation")
+    protected Dialog onCreateDialog(int id) {
+        if (id == 999) {
+            return new DatePickerDialog(this, myDateListener, year, month, day);
+        }
+        return null;
+    }
+    private DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
+            // arg1 = year
+            // arg2 = month
+            // arg3 = day
+            showDate(arg1, arg2+1, arg3);
+        }
+    };
+    private void showDate(int year, int month, int day) {
+        ((TextView) findViewById(R.id.createTaskDate)).setText(new StringBuilder().append(month).append("/")
+                .append(day).append("/").append(year));
+    }
+    //end of Select Date
 
-        myFriendList.setChoiceMode(myFriendList.CHOICE_MODE_MULTIPLE);
+    //start of image upload
+    public void UploadImage(View view){
 
-        myFriendList.setTextFilterEnabled(true);
+        final CharSequence[] options = { "Take Photo", "Choose from Gallery","Cancel" };
 
-
-        List<String> listFriendItems=new ArrayList<String>();
-
-        listFriendItems.add("Veeresh");
-        listFriendItems.add("Jorge");
-        listFriendItems.add("Yamini");
-        listFriendItems.add("Harsh");
-
-        ArrayAdapter<String> adapterFriendName = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, listFriendItems);
-        myFriendList.setAdapter(adapterFriendName);
-
-        Button btnCreateTask=(Button)findViewById(R.id.btnCreateTask);
-
-        btnCreateTask.setOnClickListener(new View.OnClickListener() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add Photo!");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent i=new Intent(getApplicationContext(),ViewTask.class);
-                startActivity(i);
+            public void onClick(DialogInterface dialog, int item) {
+                if (options[item].equals("Take Photo")) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    File f = new File(android.os.Environment.getExternalStorageDirectory(), "temp.jpg");
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                    startActivityForResult(intent, 1);
+                } else if (options[item].equals("Choose from Gallery")) {
+                    Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent, 2);
+
+                } else if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
             }
         });
+        builder.show();
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                curimage = null;
+                File f = new File(Environment.getExternalStorageDirectory().toString());
+                for (File temp : f.listFiles()) {
+                    if (temp.getName().equals("temp.jpg")) {
+                        f = temp;
+                        break;
+                    }
+                }
+                try {
+                    BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+
+                    curimage = BitmapFactory.decodeFile(f.getAbsolutePath(),
+                            bitmapOptions);
+
+                    String path = android.os.Environment
+                            .getExternalStorageDirectory()
+                            + File.separator
+                            + "Phoenix" + File.separator + "default";
+                    f.delete();
+                    OutputStream outFile = null;
+                    File file = new File(path, String.valueOf(System.currentTimeMillis()) + ".jpg");
+                    try {
+                        outFile = new FileOutputStream(file);
+                        curimage.compress(Bitmap.CompressFormat.JPEG, 85, outFile);
+                        outFile.flush();
+                        outFile.close();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else if (requestCode == 2) {
+
+                Uri selectedImage = data.getData();
+                String[] filePath = { MediaStore.Images.Media.DATA };
+                Cursor c = getContentResolver().query(selectedImage,filePath, null, null, null);
+                c.moveToFirst();
+                int columnIndex = c.getColumnIndex(filePath[0]);
+                String picturePath = c.getString(columnIndex);
+                c.close();
+                curimage = (BitmapFactory.decodeFile(picturePath));
+                Log.w("CompleteTask", picturePath + "");
+            }
+            //if image is selected
+            if(curimage != null){
+                curimage = Bitmap.createScaledBitmap(curimage, 500, 500, true);
+                ImageView iv = new ImageView(this);
+                iv.setImageBitmap(curimage);
+                iv.setPadding(10,10, 10, 10);
+                imageList.add(curimage);
+                ((LinearLayout) findViewById(R.id.createTaskImageLayout)).addView(iv);
+            }
+
+        }
+    }
+    //end of image upload
+
+    public void CreateTask(View view) {
+        Intent i = new Intent(getApplicationContext(), ViewTask.class);
+        startActivity(i);
     }
 }
