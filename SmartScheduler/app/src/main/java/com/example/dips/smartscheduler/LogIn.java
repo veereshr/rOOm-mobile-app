698daeaa871a46afb58aae2aaaa8260aaed14b07
vrangasa <rveereshwaran@gmail.com>
2016-04-09 11:20:53 -0500

    Login Module Commit

--- SmartScheduler/app/src/main/java/com/example/dips/smartscheduler/LogIn.java
+++ SmartScheduler/app/src/main/java/com/example/dips/smartscheduler/LogIn.java
@@ -1,23 +1,30 @@
 package com.example.dips.smartscheduler;
 
-import android.accounts.AccountManager;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class LogIn extends AppCompatActivity {
     EditText phnNumberTxt;
+    EditText passTextLogin;
+    int phnNumber;
+    String password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
-       phnNumberTxt= (EditText) findViewById(R.id.phoneTextLogIn);
+        findViewsById();
+
+    }
+
+    private void findViewsById() {
+        phnNumberTxt= (EditText) findViewById(R.id.phoneTextLogIn);
+        passTextLogin = (EditText) findViewById(R.id.passTextLogin);
     }
 
     public void LogIn(View v){
@@ -25,12 +32,23 @@ public class LogIn extends AppCompatActivity {
         //TODO FILL THIS IN WITH REAL USER DATA
 try {
     SharedPreferences.Editor editor = getSharedPreferences("Data", MODE_PRIVATE).edit();
-    int phnNumber = Integer.parseInt(phnNumberTxt.getText().toString());
-    editor.putInt("phoneNumber", phnNumber);
-    editor.commit();
+    phnNumber = Integer.parseInt(phnNumberTxt.getText().toString());
+    password = passTextLogin.getText().toString();
+    DatabaseHelper dbhelper = new DatabaseHelper(this);
+    String[] credentials = new String[] { phnNumber+"", password};
+    if(dbhelper.checkIfValidUser(credentials)){
+        editor.putInt("phoneNumber", phnNumber);
+        editor.commit();
+        Intent intent = new Intent(v.getContext(), GroupList.class);
+        startActivity(intent);
+    }
+    else{
+        Toast.makeText(this,"User credential is invalid",Toast.LENGTH_SHORT).show();
+    }
 
-    Intent intent = new Intent(v.getContext(), GroupList.class);
-    startActivity(intent);
+
+
+
 }catch(Exception e){
     Toast.makeText(getApplicationContext(),"Enter valid Login Credentials",Toast.LENGTH_LONG).show();
 }
@@ -40,4 +58,6 @@ try {
         Intent intent = new Intent(v.getContext(),CreateAccount.class);
         startActivity(intent);
     }
+
+
 }
